552174e3966abfcbad47812513902c416402e84b
Sathish <sathish@alethea.in>
2023-07-10 14:52:52 +0530

    Store View

---
+++ src/StoreViewer/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import StoreView from './components/StoreView';
+
+function App() {
+
+  return (
+    <>
+      <StoreView />
+    </>
+  );
+}
+
+export default App;

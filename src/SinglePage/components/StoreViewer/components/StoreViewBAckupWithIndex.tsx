a0b6ef54d124e8f009b54a2645eac16912389b12
Sathish <sathish@alethea.in>
2023-07-11 14:28:40 +0530

    Direct id will shown

--- src/SinglePage/components/StoreViewer/components/StoreViewBAckupWithIndex.tsx
+++ src/SinglePage/components/StoreViewer/components/StoreViewBAckupWithIndex.tsx
@@ -37,16 +37,22 @@ const handleCopy = (label: string) => {
 };
 
 const renderTree = (nodes: any, isRoot: boolean, i: number) => (
-    <section key={i} style={{ position: 'relative' }}>
+    <section key={i}>
         {isRoot && <button className='copy' onClick={() => handleCopy(`${nodes.id}`)}>
             {/* <img src={InfoOutlinedIcon} alt='copyButton' /> */}
             <InfoOutlinedIcon />
         </button>}
         <TreeItem key={nodes.id} nodeId={nodes.id} label={`${nodes.id}`}>
+            {console.log('nodes', nodes)}
+
             {Object.entries(nodes).map(([key, value], index) => {
                 if (key !== 'id' && key !== 'name' && key !== 'children' && key !== 'channels' && key !== 'annotations') {
                     return (
-                        <TreeItem key={`${nodes.id}-${key}`} nodeId={`${nodes.id}-${key}`} label={`${key}: ${value}`} />
+                        <>
+                            <TreeItem key={`${nodes.id}-${key}`} nodeId={`${nodes.id}-${key}`} label={`${key}: ${value}`} />
+
+                        </>
+
                     );
                 }
                 return null;
@@ -70,7 +76,10 @@ const renderTree = (nodes: any, isRoot: boolean, i: number) => (
                     {nodes.annotations.length > 0 ? (
                         nodes.annotations.map((annotation: any, index: number) => (
                             Object.entries(annotation).map(([key, value], index) => (
-                                <TreeItem key={`${nodes.id}-${key}`} nodeId={`${nodes.id}-${key}`} label={`${key}: ${value}`} />
+                                <>
+                                    <TreeItem key={`${nodes.id}-${key}`} nodeId={`${nodes.id}-${key}`} label={`${key}: ${value}`} />
+                                    {/* <button className='copy' onClick={() => handleCopy(`${key}: ${value}`)} style={{ border: "none", background: "transparent", padding: "0", cursor: "pointer" }}>i</button> */}
+                                </>
                             ))
                         ))
                     ) : (
@@ -84,13 +93,50 @@ const renderTree = (nodes: any, isRoot: boolean, i: number) => (
 );
 
 
+const transformData = (annotation: any[]): Device[] => {
+    const transformedNodes: Device[] = [];
+
+    for (const key in annotation) {
+        const element = annotation[key];
+        const children: Child[] = [];
+        const child: Child = {
+            id: element.id?.toString(),
+        };
+
+        for (const field in element) {
+            if (Array.isArray(element[field])) {
+                child[field] = element[field];
+            }
+            if (
+                field !== 'annotations' &&
+                field !== 'channels'
+            ) {
+                child[field] = element[field];
+            }
+        }
+        children.push(child);
+
+        const deviceNode: Device = {
+            id: key,
+            name: key,
+            children,
+        };
+
+        transformedNodes.push(deviceNode);
+    }
+
+    return transformedNodes;
+};
+
 const StoreView = () => {
     const [deviceNodes, setDeviceNodes] = useState<Device[]>([]);
     const [StoreConfig, setStoreConfig] = useState<Device[]>([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        setDeviceNodes(StoreConfig);
+        console.log('StoreConfig', StoreConfig);
+        const transformedNodes = transformData(StoreConfig);
+        setDeviceNodes(transformedNodes);
     }, [StoreConfig]);
 
     useEffect(() => {
@@ -116,7 +162,7 @@ const StoreView = () => {
                     defaultCollapseIcon={<ExpandMoreIcon />}
                     defaultExpandIcon={<ChevronRightIcon />}
                 >
-                    {deviceNodes.map((node, i) => renderTree(node, true, i))}
+                    {deviceNodes.map((node, i) => renderTree(node))}
                 </TreeView>
             )}
         </>

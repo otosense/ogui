d6c4cbfe14dd139996cf99a125efe3210a1fb94a
Sathish <sathish@alethea.in>
2023-07-27 11:31:02 +0530

    Updated StoreView

--- src/StoreViewer/main.tsx
+++ src/StoreViewer/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ErrorComponent from '../utilities/ErrorComponent';
+import ErrorBoundary from '../utilities/ErrorBoundary';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <ErrorBoundary fallback={<ErrorComponent />}>
+        <Suspense fallback={<h1>Loading..</h1>}>
+          <App />
+        </Suspense>
+      </ErrorBoundary>
+    </BrowserRouter>
+
+  </QueryClientProvider >
+  // </React.StrictMode>,
+);

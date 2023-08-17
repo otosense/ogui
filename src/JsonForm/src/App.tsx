919092c39a24943673bba7df152371280e6721aa
Sathish <sathish@alethea.in>
2023-08-17 10:35:17 +0530

    converting  as component

--- src/JsonForm/src/App.tsx
+++ src/JsonForm/src/App.tsx
@@ -1,15 +1,120 @@
 import React from 'react';
 import './App.css';
 import Main from './pages/Main';
-import CustomSearchable from './pages/CustomSearchable';
+import { RJSFSchema, UiSchema } from '@rjsf/utils';
+
 
 function App() {
+  const schema: RJSFSchema = {
+    type: 'object',
+    "title": "My title",
+    "description": "My description",
+    properties: {
+      firstName: {
+        type: 'string',
+        title: 'First Name',
+        minLength: 10,
+        default: "abcdefghijkl" // which set value to the input field
+      },
+      lastName: {
+        type: 'string',
+        title: 'Last Name',
+        maxLength: 4
+      },
+      age: {
+        type: 'number',
+        title: 'Age',
+      },
+      password: {
+        type: 'string',
+        title: 'Password',
+        minLength: 8, // Minimum length requirement
+        pattern: '^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!$%@#£€*?&]+$',
+        description:
+          'At least 8 characters, including at least one letter and one number.'
+      },
+      additionalItems: {
+        type: "boolean"
+      },
+      check: {
+        type: "string",
+        enum: ["one", "two", "three"]
+      },
+      multiChoiceWithCheckBox: {
+        type: "array",
+        title: "A multiple-choice list With Checkboxes",
+        items: {
+          type: "string",
+          enum: ["foo", "bar", "fuzz", "qux"],
+        },
+        uniqueItems: true
+      },
+      multiChoiceWithArray: {
+        type: "array",
+        title: "A multiple-choice list With Array",
+        items: {
+          type: "string",
+          enum: ["foo", "bar", "fuzz", "qux"],
+        },
+        uniqueItems: true
+      },
+      done: {
+        type: "string",
+        enum: ["foo", "bar", "fuzz", "qux"],
+      },
+      fileUpload: {
+        type: "string",
+        format: "data-url",
+      }
+    },
+    "required": ["firstName", "age", "additionalItems"],
+    "dependencies": {
+      "firstName": ["lastName"]
+    }
+  };
+
+  const uiSchema: UiSchema = {
+    "ui:order": ["firstName", "lastName", "age", "password", "multiChoiceWithArray", "someNumber", "check", "multiChoiceWithCheckBox", "additionalItems", "done", "fileUpload"],
+    multiChoiceWithCheckBox: {
+      "ui:widget": "checkboxes",
+      "ui:options": {
+        inline: true,
+        addable: false
+      }
+      // "ui:options": {
+
+      // },
+    },
+    fileUpload: {
+      "ui:widget": "file",
+      "ui:options": { accept: ".pdf" }
+    },
+    done: {
+      "ui:widget": "radio" // could also be "select"
+    },
+    password: {
+      'ui:widget': 'password'
+    },
+    firstName: {
+      'ui:placeholder': 'Enter your first name',
+      "ui:autofocus": true,
+      required: true
+    },
+    lastName: {
+      'ui:placeholder': 'Enter your last name',
+      'ui:readonly': true,
+    },
+    age: {
+      "ui:help": "The Age that can be used to contact you"
+    }
+  };
+
+  const handleSubmit = ({ formData }: any) => {
+    console.log('Form data submitted:', formData);
+  };
 
   return (
-    <>
-      <Main />
-      {/* <CustomSearchable /> */}
-    </>
+    <Main schema={schema} liveValidate={false} uiSchema={uiSchema} handleSubmit={handleSubmit} />
   );
 }
 
--- src/JsonForm/src/pages/Main.tsx
+++ src/JsonForm/src/pages/Main.tsx
@@ -1,136 +1,11 @@
-import React from 'react';
+import React, { memo } from 'react';
 // import Form from '@rjsf/core';
 import Form from '@rjsf/mui';
 import validator from '@rjsf/validator-ajv8';
 import { RJSFSchema, UiSchema } from '@rjsf/utils';
 
-// Note: 
-
-// input text => where user can input we can validate input (rules are per user given)
-// dependency check
-// dropdown with multiple choices =>  need tick mark => we need to do customize
-// single and multiple select checkbox
-
-
-
-// add searchable dropdown
-// add ReadOnly (not Editable)
-// customize example: input box with searchable icon, 
-// instead of highlighting the dropdown provide tick mark
-
-const schema: RJSFSchema = {
-    type: 'object',
-    "title": "My title",
-    "description": "My description",
-    properties: {
-        firstName: {
-            type: 'string',
-            title: 'First Name',
-            minLength: 10,
-            default: "Sathish" // which set value to the input field
-        },
-        lastName: {
-            type: 'string',
-            title: 'Last Name',
-            maxLength: 4
-        },
-        age: {
-            type: 'number',
-            title: 'Age',
-        },
-        password: {
-            type: 'string',
-            title: 'Password',
-            minLength: 8, // Minimum length requirement
-            pattern: '^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!$%@#£€*?&]+$',
-            description:
-                'At least 8 characters, including at least one letter and one number.'
-        },
-        additionalItems: {
-            type: "boolean"
-        },
-        check: {
-            type: "string",
-            enum: ["one", "two", "three"]
-        },
-        multiChoiceWithCheckBox: {
-            type: "array",
-            title: "A multiple-choice list With Checkboxes",
-            items: {
-                type: "string",
-                enum: ["foo", "bar", "fuzz", "qux"],
-            },
-            uniqueItems: true
-        },
-        multiChoiceWithArray: {
-            type: "array",
-            title: "A multiple-choice list With Array",
-            items: {
-                type: "string",
-                enum: ["foo", "bar", "fuzz", "qux"],
-            },
-            uniqueItems: true
-        },
-        done: {
-            type: "string",
-            enum: ["foo", "bar", "fuzz", "qux"],
-        },
-        fileUpload: {
-            type: "string",
-            format: "data-url",
-        }
-    },
-    "required": ["firstName", "age", "additionalItems"],
-    "dependencies": {
-        "firstName": ["lastName"]
-    }
-};
-
-const uiSchema: UiSchema = {
-
-    "ui:order": ["firstName", "lastName", "age", "password", "multiChoiceWithArray", "someNumber", "check", "multiChoiceWithCheckBox", "additionalItems", "done", "fileUpload"],
-    multiChoiceWithCheckBox: {
-        "ui:widget": "checkboxes",
-        "ui:options": {
-            inline: true,
-            addable: false
-        }
-        // "ui:options": {
-
-        // },
-    },
-    fileUpload: {
-        "ui:widget": "file",
-        "ui:options": { accept: ".pdf" }
-    },
-    done: {
-        "ui:widget": "radio" // could also be "select"
-    },
-    password: {
-        'ui:widget': 'password'
-    },
-    firstName: {
-        'ui:placeholder': 'Enter your first name',
-        "ui:autofocus": true,
-        required: true
-    },
-    lastName: {
-        'ui:placeholder': 'Enter your last name',
-        'ui:readonly': true,
-    },
-    age: {
-        "ui:help": "The Age that can be used to contact you"
-    }
-};
-
-const ExampleForm = () => {
-    const initialFormData = {
-        lastName: 'Smith' // Set your default value here
-    };
-    const handleSubmit = ({ formData }: any) => {
-        console.log('Form data submitted:', formData);
-    };
-
+const ExampleForm = (props: { schema: RJSFSchema; liveValidate: boolean; uiSchema: UiSchema; handleSubmit: ({ formData }: any) => void; }) => {
+    const { schema, liveValidate, uiSchema, handleSubmit } = props;
 
     return (
         <div>
@@ -138,14 +13,13 @@ const ExampleForm = () => {
             <Form
                 schema={schema}
                 uiSchema={uiSchema}
-                formData={initialFormData} // Pass initial form data
+                liveValidate={liveValidate}
                 onSubmit={handleSubmit}
                 validator={validator}
-                liveValidate={false}
                 noHtml5Validate
             />
         </div>
     );
 };
 
-export default ExampleForm;
+export default memo(ExampleForm);

f888ccb409a081b2d38ec03d4de89c98359d74d2
Sathish <sathish@alethea.in>
2023-08-25 17:09:09 +0530

    updated Readme File

--- src/DAG/Testing/App.tsx
+++ src/DAG/Testing/App.tsx
@@ -4,16 +4,12 @@ import 'reactflow/dist/style.css';
 import AppTest from './AppTest';
 
 import Dagger from '../Dagger';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-const queryClient = new QueryClient();
 function App() {
 
   return (
     // Main component Starts at Dagger 
 
-    <QueryClientProvider client={queryClient}>
-      <AppTest />
-    </QueryClientProvider>
+    <AppTest />
   );
 }
 

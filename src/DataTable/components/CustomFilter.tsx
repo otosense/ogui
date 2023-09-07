5c5a65d96024b3b75e327ab2e5667dc3ef21dbd4
Sathish <sathish@alethea.in>
2023-09-07 12:28:45 +0530

    Custom Filter

--- src/DataTable/components/CustomFilter.tsx
+++ src/DataTable/components/CustomFilter.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { map, startsWith } from "lodash";
-import ReactFilterBox, { SimpleResultProcessing, Expression } from "react-filter-box";
+import { endsWith, map, startsWith } from "lodash";
+import ReactFilterBox, { SimpleResultProcessing, Expression, GridDataAutoCompleteHandler } from "react-filter-box";
 
 interface GlobalFiltersProps {
     tableData: any;
@@ -11,7 +11,25 @@ interface GlobalFiltersProps {
 interface GlobalFiltersState {
     data: any;
     query: string;
-    isOk: boolean;
+}
+
+//extend this class to add your custom operator
+class CustomAutoComplete extends GridDataAutoCompleteHandler {
+
+    // override this method to add new your operator
+    needOperators(parsedCategory: string) {
+        var result = super.needOperators(parsedCategory);
+        return result.concat(["startsWith", "endsWith"]);
+    }
+
+    //override to custom to indicate you want to show your custom date time
+    // needValues(parsedCategory: string, parsedOperator: string) {
+    //     if (parsedOperator == "after") {
+    //         return [{ customType: "date" }];
+    //     }
+
+    // return super.needValues(parsedCategory, parsedOperator);
+    // }
 }
 
 class CustomResultProcessing extends SimpleResultProcessing {
@@ -24,7 +42,10 @@ class CustomResultProcessing extends SimpleResultProcessing {
             case "!=": return row[field] != value;
             case "contains": return row[field].toLowerCase().indexOf(value.toLowerCase()) >= 0;
             case "!contains": return row[field].toLowerCase().indexOf(value.toLowerCase()) < 0;
-            case "startsWith": return startsWith(row[field].toLowerCase(), value.toLowerCase());
+            // case "startsWith": return startsWith(row[field]?.toLowerCase(), value.toLowerCase());
+            case "startsWith": return startsWith(String(row[field]), String(value));
+            case "endsWith": return endsWith(String(row[field]), String(value));
+
         }
 
         return false;
@@ -33,35 +54,29 @@ class CustomResultProcessing extends SimpleResultProcessing {
 
 class GlobalFilters extends Component<GlobalFiltersProps, GlobalFiltersState> {
     options: any;
+    customAutoComplete: CustomAutoComplete;
 
     constructor(props: GlobalFiltersProps) {
         super(props);
-        let { tableData, flatRowData, onNewData } = props;
+        let { tableData, flatRowData } = props;
         const columnList = tableData;
         this.options = columnList
             .filter((item: { id: string; }) => item.id !== 'mrt-row-drag' && item.id !== 'mrt-row-expand' && item.id !== 'mrt-row-select' && item.id !== 'mrt-row-numbers')
             .map((item: { id: any; }) => ({
                 columnField: String(item.id).toString(),
-                type: 'selection',
             }));
         this.state = {
             data: flatRowData,
             query: "",
-            isOk: false
         };
-
+        this.customAutoComplete = new CustomAutoComplete(tableData, this.options);
     }
+
 
 
     //customer your rendering item in auto complete
     customRenderCompletionItem(self: any, data: { value: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; type: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }, registerAndGetPickFunc: () => any) {
-        // if(!_.isString(data.value)){
-        //     var pick = registerAndGetPickFunc();
-        //     return  <div className="day-picker-selection"  >
-        //         <DayPicker onDayClick={ (day: { toLocaleDateString: () => any; }) => pick(day.toLocaleDateString())  }/> </div>
-        // }
-
-        var className = ` hint-value cm-${data.type}`;
+        const className = ` hint-value cm-${data.type}`;
 
         return <div className={className}  >
             <span style={{ fontWeight: "bold" }}>{data.value}</span>
@@ -72,10 +87,10 @@ class GlobalFilters extends Component<GlobalFiltersProps, GlobalFiltersState> {
     onParseOk(expressions: Expression[]) {
 
         let { flatRowData, onNewData } = this.props;
-        console.log('expressions', expressions);
+        // console.log('expressions', expressions);
         // const newData = new SimpleResultProcessing(this.options).process(flatRowData, expressions);
         var newData = new CustomResultProcessing(this.options).process(flatRowData, expressions);
-        console.log('newData', newData);
+        // console.log('newData', newData);
         this.setState({ data: newData });
         onNewData(newData);
     }
@@ -86,6 +101,7 @@ class GlobalFilters extends Component<GlobalFiltersProps, GlobalFiltersState> {
                 <div className="main-container">
                     <ReactFilterBox
                         query={this.state.query}
+                        autoCompleteHandler={this.customAutoComplete}
                         data={this.props.flatRowData}
                         options={this.options}
                         onParseOk={this.onParseOk.bind(this)}

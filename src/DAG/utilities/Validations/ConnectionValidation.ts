2b1b126ab0a8af1763b62bb04e0b3a0731ee6864
Sathish <sathish@alethea.in>
2023-10-04 15:53:07 +0530

    connection Handlers

--- src/DAG/utilities/Validations/ConnectionValidation.ts
+++ src/DAG/utilities/Validations/ConnectionValidation.ts
@@ -1,9 +1,10 @@
-import { Edge } from "reactflow";
 import { showToast } from "../ReactToastMessage";
+import { Edge } from "reactflow";
+
 
 function connectionValidation(nodes: any[], edges: any[], setEdges: React.Dispatch<React.SetStateAction<any[]>>) {
     return (connection: any) => {
-        const { source, target, targetHandle } = connection;
+        const { source, target, targetHandle, sourceHandle } = connection;
         const sourceNode = nodes.find((node: { id: string; }) => node.id === source);
         const targetNode = nodes.find((node: { id: string; }) => node.id === target);
 
@@ -21,25 +22,32 @@ function connectionValidation(nodes: any[], edges: any[], setEdges: React.Dispat
         }
 
         const incomingConnectionsToTarget = edges.filter((edge) => edge.target === target);
+        const outgoingConnectionsFromSource = edges.filter((edge) => edge.source === source);
 
         // Find the existing incoming connection with the same target handle
         const existingIncomingEdge = incomingConnectionsToTarget.find((edge) => edge.targetHandle === targetHandle);
 
         if (existingIncomingEdge) {
-            // Replace the existing incoming edge with the new connection
-            const updatedEdges = edges.map((edge) => {
-                if (edge === existingIncomingEdge) {
-                    return { ...connection, type: "smoothstep", animated: true };
+            // Remove the existing incoming edge
+            const updatedEdges = edges.filter((edge) => edge !== existingIncomingEdge);
 
-                }
-                return edge;
-            });
-            setEdges(updatedEdges);
+            // Add the new connection
+            setEdges((prevEdges: Edge<any>[]) => [...updatedEdges, { ...connection, type: "smoothstep", animated: true }]);
         } else {
             // No existing incoming edge with the same target handle, add the new connection
             setEdges((prevEdges: Edge<any>[]) => [...prevEdges, { ...connection, type: "smoothstep", animated: true }]);
-
         }
+
+        // Remove all existing outgoing connections from the same source
+        const updatedEdgesAfterSourceRemoval = edges.filter((edge) => edge.source !== source);
+
+        // Find and remove all existing incoming connections with the same target handle
+        const updatedEdgesAfterTargetRemoval = updatedEdgesAfterSourceRemoval.filter(
+            (edge) => !(edge.target === target && edge.targetHandle === targetHandle)
+        );
+
+        // Add the new connection
+        setEdges((prevEdges: Edge<any>[]) => [...updatedEdgesAfterTargetRemoval, { ...connection, type: "smoothstep", animated: true }]);
 
         return true;
     };

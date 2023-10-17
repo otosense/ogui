d3328f366ae97ac3ed8bcde4c61002677f26a232
Sathish <sathish@alethea.in>
2023-10-17 17:34:16 +0530

    save feature

--- src/JsonForm/Testing/AppTest.tsx
+++ src/JsonForm/Testing/AppTest.tsx
@@ -86,6 +86,38 @@ function AppTest() {
         }
     };
 
+    const saveSchema = async (data: any) => {
+
+        const payload = {
+            "_attr_name": '__setitem__',
+            key: data.key,
+            value: data.value
+        };
+        console.log({ payload });
+        try {
+            const response = await fetch("http://20.219.8.178:8080/form_spec_store", {
+                method: "POST",
+                body: JSON.stringify({
+                    ...payload
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const json = await response.json();
+            // console.log('json', json);
+            return json;
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            return []; // Return an empty array or handle the error appropriately
+        }
+    };
+
 
 
     // const schema = {
@@ -117,6 +149,7 @@ function AppTest() {
     const configuration = {
         getStoreList: getFullFormSpecStore(),
         onLoadSchema: getSchemaForForm,
+        saveSchema: saveSchema,
         func: sum,
     };
     return (
--- src/JsonForm/components/Editor.tsx
+++ src/JsonForm/components/Editor.tsx
@@ -3,19 +3,27 @@ import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
 import { Alert, AppBar, Box, Divider, IconButton, MenuItem, Toolbar, Tooltip, Typography } from '@mui/material';
 import { isEmpty } from 'lodash';
 import LoadingOverlay from '../../utilities/Loader';
+import SaveIcon from '@mui/icons-material/Save';
 
 type TSchemaManager = {
     onDataUploaded?: any;
     data: any;
     title: string;
+    saveSchema?: any;
+    formType?: {
+        label: string,
+        value: string;
+    };
 };
 function Editors(props: TSchemaManager) {
-    const { title, data, onDataUploaded } = props;
+    const { title, data, onDataUploaded, saveSchema, formType } = props;
     const [errors, setErrors] = useState<any[]>([]);
     const [jsonString, setJsonString] = useState<any>({});
+    const [value, setValue] = useState((JSON.stringify(data, null, 2)));
 
     useEffect(() => {
         setJsonString(data);
+        setValue((JSON.stringify(data, null, 2)));
     }, [data]);
     function handleEditorValidation(markers: any[]) {
         const errors = markers.length > 0 ? markers : [];
@@ -23,8 +31,25 @@ function Editors(props: TSchemaManager) {
     }
 
     function handleEditorChange(value: any, event: any) {
+        setJsonString(value);
         onDataUploaded && onDataUploaded(value);
     }
+
+
+    const saveSchemas = () => {
+        console.log({ jsonString });
+        const val = {
+            rjsf: {
+                schema: jsonString
+            }
+        };
+        console.log({ val });
+        const finalPayload = {
+            value: val,
+            key: formType?.value,
+        };
+        saveSchema(finalPayload);
+    };
 
 
     function ErrorHandlers(errors: any[]) {
@@ -44,6 +69,9 @@ function Editors(props: TSchemaManager) {
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             {title}
                         </Typography>
+                        <IconButton aria-label="SaveIcon" color="inherit" onClick={saveSchemas}>
+                            <SaveIcon />
+                        </IconButton>
                     </Toolbar>
                 </AppBar>
             </Box>
@@ -52,7 +80,7 @@ function Editors(props: TSchemaManager) {
                 height='40vh'
                 language='json'
                 options={{ minimap: { enabled: false } }}
-                value={(JSON.stringify(data, null, 2))}
+                value={value}
                 onValidate={handleEditorValidation}
                 onChange={handleEditorChange}
                 loading={<LoadingOverlay />}

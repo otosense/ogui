181de04af666f61251c1870265d0dea1e1c4695d
Sathish <sathish@alethea.in>
2023-10-19 16:22:16 +0530

    fixed ts issue

--- src/JsonForm/components/Editor.tsx
+++ src/JsonForm/components/Editor.tsx
@@ -1,92 +1,90 @@
-import React, { memo, useEffect, useState } from 'react';
-import Editor, { useMonaco } from '@monaco-editor/react';
-import { Alert, AppBar, Box, Divider, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
-import LoadingOverlay from '../../utilities/Loader';
-import SaveIcon from '@mui/icons-material/Save';
+import React, { memo, useEffect, useState } from 'react'
+import Editor, { useMonaco } from '@monaco-editor/react'
+import { Alert, AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
+import LoadingOverlay from '../../utilities/Loader'
+import SaveIcon from '@mui/icons-material/Save'
 // import * as monaco from "monaco-editor";
 
-type TSchemaManager = {
-    onDataUploaded?: any;
-    data: any;
-    title: string;
-    saveSchema?: any;
-    formType?: {
-        label: string,
-        value: string;
-    };
-};
+interface TSchemaManager {
+  onDataUploaded?: any
+  data: any
+  title: string
+  saveSchema?: any
+  formType?: {
+    label: string
+    value: string
+  }
+}
 const MONACO_OPTIONS: any = {
-    autoIndent: "full",
-    automaticLayout: true,
-    contextmenu: true,
-    fontFamily: "monospace",
-    fontSize: 13,
-    lineHeight: 24,
-    hideCursorInOverviewRuler: true,
-    matchBrackets: "always",
-    minimap: {
-        enabled: false,
-    },
-    readOnly: false,
-    scrollbar: {
-        horizontalSliderSize: 4,
-        verticalSliderSize: 4,
-    },
-    selectOnLineNumbers: true,
-    roundedSelection: false,
-    cursorStyle: 'line',
-};
-function Editors(props: TSchemaManager) {
-    const { title, data, onDataUploaded, saveSchema, formType } = props;
-    const [errors, setErrors] = useState<any[]>([]);
-    const [jsonString, setJsonString] = useState<any>({});
-    const [value, setValue] = useState((JSON.stringify(data, null, 2)));
+  autoIndent: 'full',
+  automaticLayout: true,
+  contextmenu: true,
+  fontFamily: 'monospace',
+  fontSize: 13,
+  lineHeight: 24,
+  hideCursorInOverviewRuler: true,
+  matchBrackets: 'always',
+  minimap: {
+    enabled: false
+  },
+  readOnly: false,
+  scrollbar: {
+    horizontalSliderSize: 4,
+    verticalSliderSize: 4
+  },
+  selectOnLineNumbers: true,
+  roundedSelection: false,
+  cursorStyle: 'line'
+}
+function Editors (props: TSchemaManager): JSX.Element {
+  const { title, data, onDataUploaded, saveSchema, formType } = props
+  const [errors, setErrors] = useState<any[]>([])
+  const [jsonString, setJsonString] = useState<any>({})
+  const [value, setValue] = useState((JSON.stringify(data, null, 2)))
 
+  const monaco = useMonaco()
+  useEffect(() => {
+    setJsonString(data)
+    setValue((JSON.stringify(data, null, 2)))
+  }, [data])
+  function handleEditorValidation (markers: any[]): void {
+    const errors = markers.length > 0 ? markers : []
+    setErrors(errors)
+  }
 
-    const monaco = useMonaco();
-    useEffect(() => {
-        setJsonString(data);
-        setValue((JSON.stringify(data, null, 2)));
-    }, [data]);
-    function handleEditorValidation(markers: any[]) {
-        const errors = markers.length > 0 ? markers : [];
-        setErrors(errors);
+  function handleEditorChange (value: any, event: any): void {
+    setJsonString(value)
+    onDataUploaded?.(value)
+    // setting the cursor back to its normal state
+    setTimeout(() => {
+      monaco?.editor.getEditors().forEach((editor: any) => {
+        editor.setPosition({ lineNumber: event.changes[0]?.range.startLineNumber, column: +(event.changes[0]?.range.startColumn) + 1 })
+      })
+    }, 10)
+  }
+
+  const saveSchemas = (): void => {
+    const val = {
+      rjsf: jsonString
     }
-
-    function handleEditorChange(value: any, event: any) {
-        setJsonString(value);
-        onDataUploaded && onDataUploaded(value);
-        // setting the cursor back to its normal state
-        setTimeout(() => {
-            monaco?.editor.getEditors().forEach((editor: any) => {
-                editor.setPosition({ lineNumber: event.changes[0]?.range.startLineNumber, column: event.changes[0]?.range.startColumn + 1 });
-            });
-        }, 10);
+    const finalPayload = {
+      value: val,
+      key: formType?.value
     }
+    saveSchema(finalPayload)
+  }
 
-
-    const saveSchemas = () => {
-        const val = {
-            rjsf: jsonString
-        };
-        const finalPayload = {
-            value: val,
-            key: formType?.value,
-        };
-        saveSchema(finalPayload);
-    };
-
-
-    function ErrorHandlers(errors: any[]) {
-        return errors?.length > 0 && errors?.map((error: { message: string; startLineNumber: string; }, index: number) => {
-            return (
-                <Alert severity="error" key={index}>
-                    {error?.message + ' at ' + error?.startLineNumber}
-                </Alert>
-            );
-        });
+  function ErrorHandlers (errors: any[]): JSX.Element[] | null {
+    if (errors?.length > 0) {
+      return errors.map((error: { message: string, startLineNumber: string }, index: number) => (
+        <Alert severity="error" key={`error-${index}`}>
+          {error?.message} at {error?.startLineNumber}
+        </Alert>
+      ))
     }
-    return (
+    return null
+  }
+  return (
         <main>
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static">
@@ -113,9 +111,7 @@ function Editors(props: TSchemaManager) {
             }
         </main>
 
-    );
+  )
 }
 
-export default memo(Editors);
-
-
+export default memo(Editors)

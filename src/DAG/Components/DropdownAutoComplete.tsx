b3376957842f45fdce69a445e339178122ebd8d7
Sathish <sathish@alethea.in>
2023-10-20 13:48:49 +0530

    new Style dropdown

---
+++ src/DAG/Components/DropdownAutoComplete.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react'
+import Autocomplete from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
+import { Box } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import { last, initial } from 'lodash'
+import { storeMapping } from '../../JsonForm/utilities/Mapping/storeMapping'
+
+interface Option {
+  label: string
+  value: string
+}
+
+interface ISearchBox {
+  handleValue: any
+  data: any
+}
+
+function DropdownAutoComplete (props: ISearchBox): JSX.Element {
+  const { handleValue, data } = props
+  const [selectedValue, setSelectedValue] = useState<Option | null>(null)
+  const [funcLists, setFuncLists] = useState<Option[]>([])
+  const [open, setOpen] = useState(true)
+
+  useEffect(() => {
+    const conversion = storeMapping(data)
+    setFuncLists(conversion)
+  }, [data])
+
+  const selectValueFromDropDown = (
+    event: React.SyntheticEvent<Element, Event>,
+    newValue: Option | null
+  ): void => {
+    setSelectedValue(newValue)
+    handleValue(newValue?.value)
+  }
+  return (
+        <div className='autoComplete-Dropdown'>
+            <Autocomplete
+                id="form-list"
+                options={funcLists}
+                getOptionLabel={(option) => option.label}
+                // groupBy={(option) => option.label.split('.').splice(0, 3).join('.')}
+                groupBy={(option) => initial(option?.label.split('.')).join('.')}
+                value={selectedValue}
+                onChange={selectValueFromDropDown}
+                size='medium'
+                renderInput={(params) => (
+                    <TextField
+                    {...params}
+                    label="Select Node"
+                    style={{
+                      color: '#fff !important',
+                      width: '100%', // Set the width to 100% to match the parent
+                      borderColor: '#fff !important'
+                    }}
+                    onClick={() => { setOpen(true) }}
+                    InputProps={{
+                      ...params.InputProps,
+                      style: { color: '#fff !important', borderColor: '#fff !important' }
+                    }}
+                  />
+                )}
+                renderOption={(props, option) => (
+                    <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props} style={{ backgroundColor: '#1f2937', color: '#fff', width: '100%' }}>
+                        {/* {option?.label?.split('.')[3]} */}
+                        {last(option?.label?.split('.'))}
+                    </Box>
+                )}
+                open={open}
+                onOpen={() => { setOpen(true) }}
+                onClose={() => { setOpen(false) }}
+                    style={{
+                      width: '100%',
+                      color: '#fff',
+                      borderColor: '#fff'
+                    }}
+            />
+            {/* <p>Selected Value: {(selectedValue != null) ? selectedValue.value : ''}</p> */}
+        </div>
+  )
+}
+
+export default DropdownAutoComplete
--- src/DAG/Components/NodeTypes/DropDownNode/DropDownNode.tsx
+++ src/DAG/Components/NodeTypes/DropDownNode/DropDownNode.tsx
@@ -1,56 +1,55 @@
-import React, { memo, useEffect, useState } from 'react';
-import { IDropDownNode, IFlowNode } from '../../Interfaces';
-import SearchBox from '../../SearchBox';
-import SelectConfigure from './DropDownConfigure';
-import { Checkbox, FormControlLabel } from '@mui/material';
-import { usePersistentState } from '../../../utilities/persistentState';
+import React, { memo, useEffect, useState } from 'react'
+import { type IDropDownNode, type IFlowNode } from '../../Interfaces'
+import SearchBox from '../../SearchBox'
+import SelectConfigure from './DropDownConfigure'
+import { Checkbox, FormControlLabel } from '@mui/material'
+import { usePersistentState } from '../../../utilities/persistentState'
+import DropdownAutoComplete from '../../DropdownAutoComplete'
+import { isObject } from 'lodash'
 
 // funcNode Component main function starts at "DropDownNode" function below
 
-
-
-function DropDownNode(props: IDropDownNode) {
-  // which will receive all the properties from the Dagger Component 
-  const { id, data, type, sourcePosition, funcLists, isConnectable, errorMapping, flowNodes, loadParamsList } = props;
+function DropDownNode (props: IDropDownNode): JSX.Element {
+  // which will receive all the properties from the Dagger Component
+  const { id, data, type, sourcePosition, funcLists, isConnectable, errorMapping, flowNodes, loadParamsList } = props
   // console.log({ id, data, type, sourcePosition, funcLists, isConnectable, errorMapping, flowNodes });
 
-  const [selectedValue, setSelectedValue] = useState<string | undefined>(data?.label); // Selected value from the DropDown
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(data?.label) // Selected value from the DropDown
   // const [functionList, setFunctionList] = useState(funcLists); // List of available funcNodes
-  const [autogenVarNodes, setAutogenVarNodes] = usePersistentState('autogenVarNodes', true);
+  const [autogenVarNodes, setAutogenVarNodes] = usePersistentState('autogenVarNodes', true)
 
-  const toggleAutogenVarNodes = () => setAutogenVarNodes(!autogenVarNodes);
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  const toggleAutogenVarNodes = (): void => setAutogenVarNodes(!autogenVarNodes)
 
   // errorMapper => help us to find the empty nodes or node where parameter are still empty without connection
-  function errorMapper(errorMapping: any[], id: string) {
-    const errorNode = errorMapping.find((x: IFlowNode) => x.id === id);
-    return errorNode ? 'BugFuncNode' : '';
+  function errorMapper (errorMapping: any[], id: string): string {
+    const errorNode = errorMapping.find((x: IFlowNode) => x.id === id)
+    console.log({ errorNode })
+    return isObject(errorNode) ? 'BugFuncNode' : ''
   }
 
   useEffect(() => {
-    const selectedNode: any = flowNodes.find((x: { id: string; }) => x.id === id);
-    // re-mount the selected node after saving the node 
+    const selectedNode: any = flowNodes.find((x: { id: string }): boolean => x.id === id)
+    // re-mount the selected node after saving the node
     // also Dag while DAG is Loaded this is set the Func Node functions
-    setSelectedValue(selectedNode?.data?.label || data?.label);
-  }, [flowNodes]);
-
-
+    setSelectedValue(selectedNode?.data?.label || data?.label)
+  }, [flowNodes])
 
   // useEffect(() => {
-  //   // Mapping the Data for UI elements, creating structures like 
+  //   // Mapping the Data for UI elements, creating structures like
   //   // [  { value: 'add', label: 'add' },  { value: 'apply_fitted_model', label: 'apply_fitted_model' }]
   //   const result: any = listMapping(functionList);
   //   setFunctionList(result);
   // }, [functionList]);
 
-
-  const selectValueFromDropDown = (value: React.SetStateAction<string | undefined>) => {
-    setSelectedValue(value);
-  };
+  const selectValueFromDropDown = (value: React.SetStateAction<string | undefined>): void => {
+    setSelectedValue(value)
+  }
   return (
     <>
       {/* When component load initially it will show the dropdown list Once selectedValue has proper value it will make an API call to get the list of params and which is plotted in "Handle" HTML Element */}
-      {(selectedValue === "select function Node" || selectedValue === '' || selectedValue === undefined) ?
-        <div className='addNode nowheel'>
+      {(selectedValue === 'select function Node' || selectedValue === '' || selectedValue === undefined)
+        ? <div className='addNode nowheel'>
           <h3 className='titleAddNode'>Add Nodes</h3>
           <FormControlLabel
             className='autogenVarNodes'
@@ -73,11 +72,10 @@ function DropDownNode(props: IDropDownNode) {
             })}
           </select> */}
 
-          <SearchBox data={funcLists} handleValue={selectValueFromDropDown} />
+          {/* <SearchBox data={funcLists} handleValue={selectValueFromDropDown} /> */}
+          <DropdownAutoComplete data={funcLists} handleValue={selectValueFromDropDown} />
         </div>
-        :
-
-        <section className={`text-updater-node ${type} ${errorMapper(errorMapping, id)}`}>
+        : <section className={`text-updater-node ${type} ${errorMapper(errorMapping, id)}`}>
           {/* <h4 className={`nodeTitle ${type}`} title={data.label}>{data.label}</h4> */}
           <div className={`flexProps ${type}`}>
             <SelectConfigure
@@ -97,7 +95,7 @@ function DropDownNode(props: IDropDownNode) {
         </section>
       }
     </>
-  );
+  )
 }
 
-export default memo(DropDownNode);
+export default memo(DropDownNode)

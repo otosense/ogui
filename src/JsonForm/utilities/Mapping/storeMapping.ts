7a7c6e644051b5e8c1fe041874beda2c5864e2c4
Sathish <sathish@alethea.in>
2023-10-25 11:26:27 +0530

    Remove duplicate and sorting the labels

--- src/JsonForm/utilities/Mapping/storeMapping.ts
+++ src/JsonForm/utilities/Mapping/storeMapping.ts
@@ -2,14 +2,18 @@ import { map } from 'lodash'
 
 interface ITransformedData {
   label: string
-  value: any
+  value: any // Change 'any' to the appropriate type if applicable
 }
 
-export const storeMapping = (data: any): ITransformedData[] => {
-  const transformedData = map(data, item => {
-    const value = item
-    const label = item // Extract the last part after the last dot
+export const storeMapping = (data: string[]): ITransformedData[] => {
+  // Extract unique labels and sort them
+  const uniqueLabels = Array.from(new Set(data))
+  const sortedLabels = uniqueLabels.sort()
+
+  const transformedData = map(sortedLabels, (label: string) => {
+    const value = label // You can set the value to be something else if needed
     return { label, value }
   })
+
   return transformedData
 }

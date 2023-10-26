190c6db5c21d8de2dba89bed425beca44aa4c84b
Sathish <sathish@alethea.in>
2023-10-26 16:53:19 +0530

    added Scss preprocessor

--- src/JsonForm/Testing/App.tsx
+++ src/JsonForm/Testing/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import AppTest from './AppTest';
-import 'react-splitter-layout/lib/index.css';
-import './../css/JsonForm.css';
-function App() {
+import React from 'react'
+import AppTest from './AppTest'
+import 'react-splitter-layout/lib/index.css'
+// import './../css/JsonForm.css'
+import './../css/JsonForm.scss'
+function App (): JSX.Element {
   return (
     <AppTest />
-  );
+  )
 }
 
-export default App;;
+export default App

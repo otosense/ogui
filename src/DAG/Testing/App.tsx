490cbc561a23fba53131566391428807d73561c0
Sathish <sathish@alethea.in>
2023-10-30 14:02:14 +0530

    fixed css

--- src/DAG/Testing/App.tsx
+++ src/DAG/Testing/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import './../css/Dag.css';
-import 'reactflow/dist/style.css';
-import 'react-splitter-layout/lib/index.css';
-import AppTest from './AppTest';
+import React from 'react'
+import './../css/Dag.scss'
+// import './../css/Dag.css';
+import 'reactflow/dist/style.css'
+import 'react-splitter-layout/lib/index.css'
+import AppTest from './AppTest'
 
-import Dagger from '../Dagger';
-function App() {
-
+import Dagger from '../Dagger'
+function App () {
   return (
-    // Main component Starts at Dagger 
+    // Main component Starts at Dagger
     <>
       <AppTest />
     </>
-  );
+  )
 }
 
-export default App;
-
+export default App

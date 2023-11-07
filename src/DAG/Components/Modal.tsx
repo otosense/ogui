b9fb683bbe036c761dcb9423f0b859dd9d175550
Sathish <sathish@alethea.in>
2023-11-07 18:29:55 +0530

    Fixed CSs Bug

--- src/DAG/Components/Modal.tsx
+++ src/DAG/Components/Modal.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Modal, Box, Typography, Button, Paper } from '@mui/material';
+import React from 'react'
+import { Modal, Box, Typography, Button, Paper } from '@mui/material'
 
 const CustomModal = ({ open, handleClose, title, content }: any) => {
-    return (
+  return (
         <Modal open={open} onClose={handleClose}>
             <Box
                 sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 400,
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  width: 400
                 }}
             >
                 <Paper elevation={3} sx={{ padding: 1 }}>
                     <h3 className="ModalTitle"> {title}</h3>
-                    <Typography variant="body2" gutterBottom>
+                    {/* <Typography variant="body2" gutterBottom> */}
                         {content}
-                    </Typography>
+                    {/* </Typography> */}
                 </Paper>
             </Box>
         </Modal>
-    );
-};
+  )
+}
 
-export default CustomModal;
+export default CustomModal

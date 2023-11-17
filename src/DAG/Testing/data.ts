41230c3a52af90f7ace210b8c871972a5afba559
Sathish <sathish@alethea.in>
2023-11-17 17:26:15 +0530

    DAG

--- src/DAG/Testing/data.ts
+++ src/DAG/Testing/data.ts
@@ -1,114 +1,65 @@
 export const loadDagFuncList = [
-    [
-        "funcs",
-        "apply_fitted_model"
-    ],
-    [
-        "funcs",
-        "assert_dims"
-    ],
-    [
-        "funcs",
-        "clean_dict"
-    ],
-    [
-        "funcs",
-        "consecutive_indices"
-    ],
-    [
-        "funcs",
-        "create_kfold_matrix"
-    ],
-    [
-        "funcs",
-        "dataset_train_test_split"
-    ],
-    [
-        "funcs",
-        "dflt_featurizer"
-    ],
-    [
-        "funcs",
-        "dflt_filter"
-    ],
-    [
-        "funcs",
-        "fixed_step_chunker"
-    ],
-    [
-        "funcs",
-        "get_groups_extremities_all"
-    ],
-    [
-        "funcs",
-        "indices_for_percentile"
-    ],
-    [
-        "funcs",
-        "k_fold_scores"
-    ],
-    [
-        "funcs",
-        "learn_outlier_model"
-    ],
-    [
-        "funcs",
-        "mk_chunker_step"
-    ],
-    [
-        "funcs",
-        "mk_featurizer_step"
-    ],
-    [
-        "funcs",
-        "mk_pipeline"
-    ],
-    [
-        "funcs",
-        "mk_simple_model"
-    ],
-    [
-        "funcs",
-        "mk_step"
-    ],
-    [
-        "funcs",
-        "modify_pipeline"
-    ],
-    [
-        "funcs",
-        "modify_step"
-    ],
-    [
-        "funcs",
-        "name_of_obj"
-    ],
-    [
-        "funcs",
-        "projector"
-    ],
-    [
-        "funcs",
-        "scores_to_intervals"
-    ],
-    [
-        "funcs",
-        "simple_chunker"
-    ],
-    [
-        "funcs",
-        "simple_featurizer"
-    ],
-    [
-        "funcs",
-        "tagged_sounds_to_single_array"
-    ],
-    [
-        "dags",
-        "sample_1"
-    ],
-    [
-        "dags",
-        "test_dag_sprint_review"
+  [
+    'funcs',
+    'data.execute'
+  ],
+  [
+    'dags',
+    'step_7__train_and_save_sms_recognition_dpp'
+  ]
+]
+
+export const onloadSavedDagData = {
+  name: 'step_1__show_sms_table_from_azure',
+  func_nodes: [
+    {
+      name: 'xls_bytes_',
+      func_label: 'xls_bytes',
+      out: 'xls_bytes',
+      func: 'olab.objects.store.get_item',
+      bind: {
+        key: 'path_to_xls_file',
+        store: 'golden_samples'
+      }
+    },
+    {
+      name: 'xls_table_',
+      func_label: 'xls_table',
+      out: 'xls_table',
+      func: 'olab.objects.file.read_simple_excel',
+      bind: {
+        file_bytes: 'xls_bytes'
+      }
+    },
+    {
+      name: 'table_display_',
+      func_label: 'table_display',
+      out: 'table_display',
+      func: 'olab.objects.table.display',
+      bind: {
+        table: 'xls_table'
+      }
+    },
+    {
+      name: 'golden_samples_',
+      func_label: 'golden_samples',
+      out: 'golden_samples',
+      func: 'olab.objects.projects.sms.golden_samples',
+      bind: {}
+    }
+  ]
+}
+
+export const executeData = {
+  name: 'execute',
+  signature: {
+    parameters: [
+      {
+        name: 'dpp'
+      },
+      {
+        name: 'file_bytes'
+      }
     ]
-];
+  }
+}
